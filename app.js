eacd8bca012315efeb7ec0d194288e423989b50b
Syed Faheem Shah <146163449+faheemsyed54@users.noreply.github.com>
2023-09-27 12:26:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    let x = document.getElementsByClassName('myLink')[0];
+    if (x.style.display === 'block')
+    {
+        x.style.display = 'none';
+    }else
+    {
+    x.style.display = 'block';
+    }
+}
